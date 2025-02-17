964862e0e1593575269c10279fa3c506defdf137
chenz <crazychenz@gmail.com>
2025-02-17 10:12:37 -0500

    Cleaned up old commands

--- collector/bundle.sh
+++ collector/bundle.sh
@@ -8,7 +8,7 @@ SCRIPT_NAME=x64-linux_config_install.sh
 TARFLAG="" # optionally add compression flag (gzip=>-z, bzip2=>-j, xz=>-J)
 cat >${SCRIPT_NAME} <<SH_EOF
 #!/bin/sh
-export DEST=home_config_backup-\$(date +%Y%m%d-%H%M%S) ; mkdir \${DEST} ; rsync -a .bash-user-settings.sh \${DEST}/ ; rsync -a .tmux.conf \${DEST}/ ; rsync -a .config/ \${DEST}/.config/ ; rsync -a .local/ \${DEST}/.local/ ; rsync -a .tmux/ \${DEST}/.tmux/ ; rsync -a .cache/ \${DEST}/.cache/
+export DEST=home_config_backup-\$(date +%Y%m%d-%H%M%S) ; mkdir \${DEST} ; rsync -a .local/ \${DEST}/.local/
 sed '1,10d' \$0 | tar --strip-components=2 -x
 [ -e ".bashrc" -a -z "\$(grep 'bash-user-settings.sh' .bashrc)" ] && echo "source ~/.bash-user-settings.sh" >> .bashrc
 [ ! -z "\$(ldd --version | grep -i -e gnu -e glibc)" ] && echo "export PATH=~/.local/nvim-linux64/bin:\\\$PATH" >> .bash-user-settings.sh
