fe7b680a391a42853b06351b11895e07aae26318
chenz <crazychenz@gmail.com>
2025-02-18 17:35:56 -0500

    Added cmp overrides to nvim configs

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -5,6 +5,34 @@ return {
     opts = require "configs.conform",
   },
 
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,  -- Load immediately
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1 -- Disable default keybindings (optional)
+    end
+  },
+
+
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local cmp = require("cmp")
+      local conf = require("nvchad.configs.cmp")
+
+      local mymappings = {
+        ["<C-y>"] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Insert,
+          select = true,
+        },
+        ["<CR>"] = false,
+      }
+      conf.mapping = vim.tbl_deep_extend("force", conf.mapping, mymappings)
+      return conf
+    end,
+  },
+
+
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
