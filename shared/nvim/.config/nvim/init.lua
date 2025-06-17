4f0a62ba3d2d1dcfdae4c2fb160718f15b1d2460
Crazy Chenz <crazychenz@gmail.com>
2025-06-17 21:02:56 +0100

    Update init.lua

--- shared/nvim/.config/nvim/init.lua
+++ shared/nvim/.config/nvim/init.lua
@@ -1228,3 +1228,4 @@ vim.opt.termguicolors = true
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+vim.api.nvim_set_hl(0, "Whitespace", { fg = "#2c2c2c", ctermfg = 8 })
