4d4652342ec438b03dc75754990fe26d84858bdd
chenz <crazychenz@gmail.com>
2025-09-23 06:09:59 -0400

    profile not detects dbus and desktop session as GUI. Added .config/autostart reference for ssh-add. Tested and working

--- shared/bash/.bash-user-profile.sh
+++ shared/bash/.bash-user-profile.sh
@@ -1,14 +1,18 @@
 
-if [ -n "$DISPLAY" ] || [ -n "$WAYLAND_DISPLAY" ]; then
+if [ -n "$DISPLAY" ] \
+|| [ -n "$WAYLAND_DISPLAY" ] \
+|| [ -n "$DESKTOP_SESSION" ] \
+|| [ -n "$DBUS_SESSION_BUS_ADDRESS" ]; then
     # Do GUI only things.
+    echo;
 else
     # Do console only things.
-
+    echo;
     # Start ssh-agent if not already running
     if ! pgrep -u "$USER" ssh-agent > /dev/null; then
       eval "$(ssh-agent -s)" > /dev/null
     fi
 
-    # Add our ssh key to agent.
+    ## Add our ssh key to agent.
     ssh-add -l > /dev/null 2>&1 || ssh-add ~/.ssh/id_rsa
 fi
