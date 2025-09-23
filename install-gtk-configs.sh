61687adeba694ae82ed57a6ac5a56d8326ea95a4
chenz <crazychenz@gmail.com>
2025-09-23 04:53:34 -0400

    Added gtk dotfiles folder with wezterm and zen

---
+++ install-gtk-configs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# You need to install python3, which, tput, c compiler, ncurses-utils.
+# You may need to `pip install pyyam`.
+
+set -e
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pushd "${BASEDIR}"
+  "${BASEDIR}/dotbot/bin/dotbot" -d "${BASEDIR}" -c "gtk-dotfiles.conf.yaml" "${@}"
+popd
+
+if [ ! -z "$(which update-desktop-database)" ]; then
+  update-desktop-database ~/.local/share/applications/
+fi
+
+if [ ! -z "$(which kbuildsycoca6)" ]; then
+  kbuildsycoca6 --noincremental
+fi
+
+
+
+
